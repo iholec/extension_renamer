9c2205903c33a627237c3bace7d7ebd487171334
Chaoskatze <irene.holec@technikum-wien.at>
2018-08-19 12:57:09 +0200

    first working version

---
+++ Extension_Renamer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Renamer renamer;
+                if (args.Length > 2)
+                {
+                    renamer = new Renamer(args[0], args[1], args[2]);
+
+                }
+                else
+                {
+                    renamer = new Renamer(args[0], args[1]);
+                }
+                renamer.Rename();
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("To use this programm please input the following Parameters:");
+                Console.WriteLine("[currExtension] The file extension you want to be renamed, with dot (e.g. '.png')");
+                Console.WriteLine("[newExtension] The new file extension, with dot");
+                Console.WriteLine("[filePath] The path to the files to be changed. If empty, the current directory will be used");
+                Console.WriteLine("Extension_Renamer.exe currExtension newExtension filePath");
+
+            }
+
+            String Test = Console.ReadLine();
+        }
+    }
+}
---
+++ Extension_Renamer/Renamer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Renamer
+{
+
+    class Renamer
+    {
+        private String _currExtension;
+        private String _newExtension;
+        private String _filePath;
+
+        public Renamer (String currExtension, String newExtension, String filePath){
+            _currExtension = currExtension;
+            _newExtension = newExtension;
+            _filePath = filePath;
+        }
+
+        public Renamer(String currExtension, String newExtension)
+        {
+            _currExtension = currExtension;
+            _newExtension = newExtension;
+            _filePath = Directory.GetCurrentDirectory();
+        }
+
+        public void Rename()
+        {
+            foreach (string fileName in Directory.GetFiles(_filePath))
+            {
+                //Console.WriteLine(fileName);
+                //Console.WriteLine(Path.GetExtension(fileName));
+                if (Path.GetExtension(fileName).Equals(_currExtension))
+                {
+                    String newFileName = _filePath + "\\" + Path.GetFileNameWithoutExtension(fileName) + _newExtension;
+                    System.IO.File.Move(fileName, newFileName);
+                    Console.WriteLine(fileName + " renamed to " + newFileName);
+                }
+            }
+        }
+    }
+}
